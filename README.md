dcc5e0f67b5e42fb0c3c97b1bf0c9da4a7d53b7c
shossain7757 <71143213+shossain7757@users.noreply.github.com>
2022-02-01 10:39:51 -0500

    update on Readme
    fill it with content

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 # progdata-class
 
-The repository includes all the R codes of the course PSY4931. The course instructor is *Professor Brenton M. Wiernik*
+The repository includes all the practice R codes and deliverables for the course PSY4931. The course instructor is *Professor Brenton M. Wiernik*
 
+# About Myself
+
+I am Shafayet Shariar Hossain and I am a PhD student at the Big Data Analytics Program at USF. I have completed both my Bachelor's and Master's in Statistics. I hope to consolidate my knowledge in **R** through this course and learn how to use github for collaboration.
+
+# Navigation 
+
+The repository is divided into different folders. Each folder is a different deliverable for this course.The folders are as follows
+
+  - Getting-started
+  - Lab Activity
+  - Portfolio Pieces
+  - Presentation
--- plotting_ggplot.R
+++ plotting_ggplot.R
@@ -12,5 +12,45 @@ ggplot(gapminder)+
 
 
 
+mauna <- tsibble::as_tsibble(co2) %>%
+  rename(month = index, conc = value)
 
+
+ggplot(mauna) +
+  aes(x = month, y = conc)+
+  geom_line() + 
+  tsibble::scale_x_yearmonth()
+  
+  
+# color in global aes and in geom point aes layer. 
+
+ggplot(palmerpenguins::penguins)+
+  aes(x = body_mass_g, y = bill_depth_mm, color = species)+
+  geom_point()+
+  geom_smooth(method = 'lm')
+
+
+ggplot(palmerpenguins::penguins)+
+  aes(x = body_mass_g, y = bill_depth_mm)+
+  geom_point(aes(color = species))+
+  geom_smooth(method = 'lm')
+
+
+
+p <- ggplot(mauna) +
+  aes(x = month, y = conc)+
+  geom_line() + 
+  tsibble::scale_x_yearmonth()
+
+
+p +
+  geom_smooth(color = 'darkgreen')
+
+
+ggplot(gapminder) +
+  geom_point(aes(x = gdpPercap, y = lifeExp), alpha = 0.1)
+
+
+ggplot(cars) +
+  geom_point(aes(x = speed, y = dist), color = "blue")
 
